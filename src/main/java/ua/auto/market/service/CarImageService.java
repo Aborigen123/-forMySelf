7cf8ee0bae79288463d8d2d01b4450d79df198e0
Vito Hard <igorchelsae1905@gmail.com>
2020-09-30 19:52:02 +0300

    #1

---
+++ src/main/java/ua/auto/market/service/CarImageService.java
@@ -0,0 +1,18 @@
+package ua.auto.market.service;
+
+import java.util.List;
+
+import ua.auto.market.entity.CarImage;
+
+public interface CarImageService {
+
+	void saveCarImage(CarImage carImage);
+	
+	List<CarImage> findAllCarImage();
+	
+	CarImage findImageById(Long id);
+	
+	CarImage findCarImageById(long id);
+	
+	CarImage deleteCarImageById(Long id);
+}
---
+++ src/main/java/ua/auto/market/service/impl/CarImageServiceImpl.java
@@ -0,0 +1,48 @@
+package ua.auto.market.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ua.auto.market.entity.CarImage;
+import ua.auto.market.repository.CarImageRepository;
+import ua.auto.market.service.CarImageService;
+
+@Service
+public class CarImageServiceImpl implements CarImageService{
+
+	
+	@Autowired private CarImageRepository carImageRepository;
+	
+	@Override
+	public void saveCarImage(CarImage carImage) {
+	 carImageRepository.save(carImage);
+		
+	}
+
+	@Override
+	public List<CarImage> findAllCarImage() {
+		
+		return carImageRepository.findAll();
+	}
+
+	@Override
+	public CarImage findImageById(Long id) {
+		
+		return carImageRepository.findOne(id);
+	}
+
+	@Override
+	public CarImage findCarImageById(long id) {
+		
+		return carImageRepository.findOne(id);
+	}
+
+	@Override
+	public CarImage deleteCarImageById(Long id) {
+	
+		return carImageRepository.deleteCarImageById(id);
+	}
+
+}
